ee3e374b2c986d2f31f813c6959b1555a2addfbb
Olga Smirnova <o.v.smirnova1987@gmail.com>
2016-12-23 01:21:10 +0200

    add new html file, test functions

---
+++ main.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Olga on 12/23/16.
+ */
+$(document).ready(function(){
+    var cityData = "url: document.json";
+
+
+    data = JSON.parse(cityData);
+    console.log(data);
+
+});
